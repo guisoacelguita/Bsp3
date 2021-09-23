d12c8b94a949f3863618e68c02ad2c6242b482cc
violeta.cautin <violeta.cautin@probando.io>
2021-09-23 20:58:39 +0200

    Initial commit

---
+++ script/main.js
@@ -0,0 +1,24 @@
+"use strict";
+
+
+
+const responseMessage = { 
+    "Player": "Player 1", 
+    "Cards": [ 
+        { "Color": "Red", "Text": "Three", "Value": 3, "Score": 3 }, 
+        { "Color": "Red", "Text": "Five", "Value": 5, "Score": 5 }, 
+        { "Color": "Red", "Text": "Nine", "Value": 9, "Score": 9 }, 
+        { "Color": "Red", "Text": "Reverse", "Value": 12, "Score": 20 }, 
+        { "Color": "Blue", "Text": "Two", "Value": 2, "Score": 2 }, 
+        { "Color": "Blue", "Text": "Four", "Value": 4, "Score": 4 }, 
+        { "Color": "Blue", "Text": "Five", "Value": 5, "Score": 5 }, 
+        { "Color": "Blue", "Text": "Seven", "Value": 7, "Score": 7 }, 
+        { "Color": "Blue", "Text": "Eight", "Value": 8, "Score": 8 } 
+    ], 
+    "Score": 63 };
+
+let color = prompt("What color do you want? (Choose Red or Blue)");
+let value = Number(prompt("What value do you want? (Choose a number 1-12"));
+
+let selectedCards = responseMessage.Cards.filter (item => item.Color === color && item.Value === value) ;
+    console.log(selectedCards);
